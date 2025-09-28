8521b3f1992ce784da89504d9bd2ed7fb6a488e3
phuongnguyenth0905 <phuongnguyen.th0905@gmail.com>
2025-09-28 22:44:06 +0700

    multiple browser

--- actions/commons/Basetest.java
+++ actions/commons/Basetest.java
@@ -1,5 +1,34 @@
 package commons;
 
-public class Basetest {
+import java.time.Duration;
+import java.util.Random;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Basetest {
+	WebDriver driver;
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please input the browser name!");
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		return driver;
+
+	}
+
+	protected int getRandom() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
 }
